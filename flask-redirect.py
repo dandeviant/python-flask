0cd6ec5102608ae36f522b33bb36663be5f79ad0
Matt Daniel <68473358+dandeviant@users.noreply.github.com>
2022-02-26 22:02:46 +0800

    Add files via upload

---
+++ flask-redirect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__) # declare Flask object from flask library
+
+@app.route('/') #Python decorator
+@app.route('/<name>') #Python decorator with variable
+
+def home(name=None):
+	# Front page of Flask
+	return render_template('basepage.html', name=name)
+
+if __name__ == '__main__':
+	app.run(debug=True)
